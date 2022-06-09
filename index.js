bdf0788ec903228c0d08869579d6d238153b45f9
Bryan Bosire <bryanbosire@gmail.com>
2022-06-09 16:23:31 +0300

    commit tree 9e7e986222b8af62f3c43f1ac80ac35b92b56219

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
 // Code your solution in this file!
+//return first two drivers
+const returnFirstTwoDrivers = (drivers) =>{
+    let newDrivers = [...drivers];
+    return newDrivers.slice(0,2);
+}
+
+//returns last two drivers
+const returnLastTwoDrivers = (drivers) =>{
+    let newDrivers = [...drivers];
+    return newDrivers.slice(-2);
+}
+
+// Array containing two elements from previously defined two functions
+const selectingDrivers = [returnFirstTwoDrivers,returnLastTwoDrivers];
+
+//function that will multiply a fare for a ride accordingly
+const createFareMultiplier =(value) =>{
+    return function (fare){
+        return fare * value;
+    }
+}
+
+//function accepts a fare as its lone argument and doubles it.
+const fareDoubler = createFareMultiplier(2);
+
+//function accepts a fare as its lone argument and triples it.
+const fareTripler = createFareMultiplier(3);
+
+// return either the first two drivers or the last two drivers.
+function selectDifferentDrivers(drivers,selectingDrivers){
+    return selectingDrivers(drivers);
+}
